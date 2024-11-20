07671c814a2c34689f05f5fb85fc6a4f84aee61a
rafaeelricco <rafaelricco10@gmail.com>
2024-11-20 15:27:52 -0300

    refactor: update benefits section in home page component

--- src/features/home/components/benefits.tsx
+++ src/features/home/components/benefits.tsx
@@ -3,18 +3,45 @@ import React from 'react'
 export const Benefits: React.FC = () => {
    return (
       <React.Fragment>
-         <div className="relative overflow-hidden w-full -mt-12 md:pt-20 lg:pt-32 pb-56 container">
-            <div className="grid grid-rows-3 gap-4">
-               <div className="grid grid-cols-[45%_35%_auto] gap-4 h-[372px]">
-                  <div id="box-1" className="bg-support06 w-full h-full rounded-3xl py-4  px-6"></div>
-                  <div id="box-2" className="bg-support06 w-full h-full rounded-3xl py-4 px-6"></div>
-                  <div id="box-3" className="bg-support06 w-full h-full rounded-3xl py-4  px-6"></div>
+         <div className="relative overflow-hidden w-full -mt-12 pt-32 pb-56 container">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+               <div className="grid gap-4">
+                  <div>
+                     <div className="bg-support06 w-full h-[290px] rounded-xl shadow"></div>
+                  </div>
+                  <div>
+                     <div className="bg-support06 w-full h-[290px] rounded-xl shadow"></div>
+                  </div>
+                  <div>
+                     <div className="bg-support06 w-full h-[174px] rounded-xl shadow"></div>
+                  </div>
                </div>
-
-               <div className="grid grid-cols-[45%_35%_auto] gap-4 h-[335px]">
-                  <div id="box-4" className="bg-support06 w-full h-full rounded-3xl py-4  px-6"></div>
-                  <div id="box-5" className="bg-support06 w-full h-full rounded-3xl py-4  px-6"></div>
-                  <div id="box-6" className="bg-support06 w-full h-full rounded-3xl py-4  px-6"></div>
+               <div className="grid gap-4">
+                  <div>
+                     <div className="bg-support06 w-full h-[155px] rounded-xl shadow"></div>
+                  </div>
+                  <div>
+                     <div className="bg-support06 w-full h-[349px] rounded-xl shadow"></div>
+                  </div>
+                  <div>
+                     <div className="bg-support06 w-full h-[250px] rounded-xl shadow"></div>
+                  </div>
+               </div>
+               <div className="grid gap-4">
+                  <div>
+                     <div className="bg-support06 w-full h-[349px] rounded-xl shadow"></div>
+                  </div>
+                  <div>
+                     <div className="bg-support06 w-full h-[155px] rounded-xl shadow"></div>
+                  </div>
+                  <div>
+                     <div className="bg-support06 w-full h-[250px] rounded-xl shadow"></div>
+                  </div>
+               </div>
+               <div className="flex flex-col gap-4">
+                  <div className="bg-support06 w-full min-h-[309px] rounded-xl shadow"></div>
+                  <div className="bg-support06 w-full min-h-[155px] rounded-xl shadow"></div>
+                  <div className="bg-support06 w-full h-full rounded-xl shadow"></div>
                </div>
             </div>
          </div>
