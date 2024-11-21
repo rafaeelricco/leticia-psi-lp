49bc1208cdbef55a9d644e4bc985f4d62c7ba0a3
rafaeelricco <rafaelricco10@gmail.com>
2024-11-21 12:27:04 -0300

    refactor: update hero component in home page
    - Remove unused import of `cn` from `hero.tsx`
    - Remove unused state and effect related to Safari browser detection
    - Update font size and alignment of hero title
    - Update text content of hero paragraphs

--- src/features/home/components/hero.tsx
+++ src/features/home/components/hero.tsx
@@ -1,4 +1,3 @@
-import { cn } from '@/lib/utils'
 import { WhatsAppIcon } from '@/src/components/icons/hero'
 import { Button } from '@/src/components/ui/button'
 
@@ -7,54 +6,28 @@ import Image from 'next/image'
 import React from 'react'
 
 export const Hero: React.FC = () => {
-   const [isSafari, setIsSafari] = React.useState<boolean | null>(null)
-
-   React.useEffect(() => {
-      setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent))
-   }, [])
-
    return (
       <React.Fragment>
          <div className="min-h-svh flex flex-col justify-between relative" id="hero">
             <div className="flex-1 flex items-end md:items-center container px-4 pb-12 md:pb-0 md:px-0">
                <div className="flex flex-col gap-6 w-full">
                   <div className="flex flex-col gap-2">
-                     <h1
-                        className={cn(
-                           'text-3xl text-center md:text-left text-white md:text-5xl lg:text-6xl font-thin font-forum tracking-tight',
-                           { 'opacity-0': isSafari === null }
-                        )}
-                     >
+                     <h1 className="text-3xl text-center md:text-left text-white md:text-5xl lg:text-6xl font-thin font-forum tracking-tight">
                         <span className="uppercase">Buscar ajuda é um</span>
                         <br className="hidden md:block" />
                         <span className="flex items-center gap-4 justify-center md:justify-start h-[33px] md:h-[48px]">
                            <span className="uppercase">ato de</span>
-                           <span
-                              className={cn(
-                                 'font-retro-signature text-[100px] md:text-[120px] lg:text-[140px] flex leading-[0] h-[33px] md:h-[48px]',
-                                 isSafari && 'items-center'
-                              )}
-                           >
+                           <span className="font-retro-signature text-[100px] md:text-[120px] lg:text-[140px] flex leading-[0] h-[33px] md:h-[48px] items-center">
                               coragem,
                            </span>
                         </span>
                         <span className="uppercase">e não um sinal</span> <br className="hidden md:block" />
                         <span className="uppercase">de fraqueza.</span>
                      </h1>
-                     <p
-                        className={cn(
-                           'text-sm hidden md:block sm:text-base lg:text-lg font-medium text-gray-paragraph text-center md:text-left text-white font-gadugi',
-                           { 'opacity-0': isSafari === null }
-                        )}
-                     >
+                     <p className="text-sm hidden md:block sm:text-base lg:text-lg font-medium text-gray-paragraph text-center md:text-left text-white font-gadugi">
                         Dê um passo na direção da vida que você deseja viver!
                      </p>
-                     <p
-                        className={cn(
-                           'text-sm block md:hidden sm:text-base lg:text-lg font-medium text-gray-paragraph text-center md:text-left text-white font-gadugi',
-                           { 'opacity-0': isSafari === null }
-                        )}
-                     >
+                     <p className="text-sm block md:hidden sm:text-base lg:text-lg font-medium text-gray-paragraph text-center md:text-left text-white font-gadugi">
                         Cuide de você com quem entende e acolhe.
                      </p>
                   </div>
