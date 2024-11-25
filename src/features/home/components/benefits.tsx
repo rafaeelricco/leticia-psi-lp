a04ef25778ff1cca615da6e03a2b225fc62579a7
rafaeelricco <rafaelricco10@gmail.com>
2024-11-25 20:36:45 -0300

    refactor: update benefits component layout for responsiveness in home page

--- src/features/home/components/benefits.tsx
+++ src/features/home/components/benefits.tsx
@@ -5,51 +5,51 @@ export const Benefits: React.FC = () => {
       <React.Fragment>
          <div className="relative overflow-hidden w-full -mt-12 pt-32 pb-56 container">
             <div className="grid gap-4 w-full [&>*]:w-full">
-               {/* row 1 */}
-               <div className="grid grid-cols-[2fr_1fr_1fr] grid-rows-[378px] gap-4 [&>*]:w-full">
-                  <div className="bg-support06 rounded-3xl shadow min-w-0"></div>
-                  <div className="bg-support06 rounded-3xl shadow min-w-0"></div>
-                  <div className="grid gap-4 min-w-0">
-                     <div className="bg-support06 rounded-3xl shadow"></div>
-                     <div className="bg-support06 rounded-3xl shadow"></div>
+               {/* row 1 - Ajustado grid-rows para MD */}
+               <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-[auto_auto] lg:grid-rows-[378px] lg:grid-cols-[2fr_1fr_1fr] gap-4 [&>*]:w-full">
+                  <div className="bg-support06 rounded-3xl shadow min-w-0 min-h-[200px] md:min-h-[300px] lg:min-h-0"></div>
+                  <div className="bg-support06 rounded-3xl shadow min-w-0 min-h-[200px] md:min-h-[300px] lg:min-h-0"></div>
+                  <div className="grid grid-cols-2 md:grid-cols-2 md:col-span-2 lg:col-span-1 lg:grid-cols-1 gap-4 min-w-0">
+                     <div className="bg-support06 rounded-3xl shadow min-h-[100px] md:min-h-[145px] lg:min-h-0"></div>
+                     <div className="bg-support06 rounded-3xl shadow min-h-[100px] md:min-h-[145px] lg:min-h-0"></div>
                   </div>
                </div>
 
-               {/* row 2 */}
-               <div className="grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[448px] gap-4 [&>*]:w-full">
-                  <div className="grid gap-4">
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                     <div className="grid grid-flow-col gap-4">
-                        <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                        <div className="bg-support06 h-full rounded-3xl shadow"></div>
+               {/* row 2 - Ajustado grid para MD */}
+               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-rows-[auto_auto] lg:grid-rows-[448px] lg:grid-cols-4 gap-4 [&>*]:w-full">
+                  <div className="grid gap-4 md:col-span-2 lg:col-span-1">
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
+                     <div className="grid grid-cols-2 gap-4">
+                        <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
+                        <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                      </div>
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                   </div>
 
-                  <div className="grid gap-4 grid-rows-[2fr_1fr]">
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
+                  <div className="grid gap-4 grid-rows-[2fr_1fr] md:col-span-1 lg:col-span-1">
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                   </div>
 
-                  <div className="grid gap-4 grid-rows-[2fr_1fr]">
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
+                  <div className="grid gap-4 grid-rows-[2fr_1fr] md:col-span-1 lg:col-span-1">
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                   </div>
 
-                  <div className="grid gap-4 grid-rows-[0.3fr_auto]">
+                  <div className="grid gap-4 grid-rows-[0.3fr_auto] md:col-span-2 lg:col-span-1">
                      <div className="grid grid-flow-col gap-4 h-full">
-                        <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                        <div className="bg-support06 h-full rounded-3xl shadow"></div>
+                        <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
+                        <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                      </div>
-                     <div className="bg-support06 h-full rounded-3xl shadow"></div>
+                     <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                   </div>
                </div>
 
-               {/* row 3 */}
-               <div className="grid grid-cols-[2fr_1.5fr_0.5fr] grid-rows-[138px] gap-4 [&>*]:w-full">
-                  <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                  <div className="bg-support06 h-full rounded-3xl shadow"></div>
-                  <div className="bg-support06 h-full rounded-3xl shadow"></div>
+               {/* row 3 - Ajustado grid para MD */}
+               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 md:grid-rows-[auto] lg:grid-rows-[138px] lg:grid-cols-[2fr_1.5fr_0.5fr] gap-4 [&>*]:w-full">
+                  <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] md:col-span-2 lg:col-span-1 lg:min-h-0"></div>
+                  <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
+                  <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                </div>
             </div>
          </div>
