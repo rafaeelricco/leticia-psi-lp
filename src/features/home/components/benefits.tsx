50c229503dfc20fae0441bb1602300d41c442951
rafaeelricco <rafaelricco10@gmail.com>
2024-11-27 18:49:44 -0300

    refactor: update benefits component layout for responsiveness in home page

--- src/features/home/components/benefits.tsx
+++ src/features/home/components/benefits.tsx
@@ -6,7 +6,7 @@ export const Benefits: React.FC = () => {
          <div className="relative overflow-hidden w-full -mt-12 pt-32 pb-56 container">
             <div className="grid gap-4 w-full [&>*]:w-full">
                {/* row 1 - Ajustado grid-rows para MD */}
-               <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-[auto_auto] lg:grid-rows-[378px] lg:grid-cols-[2fr_1fr_1fr] gap-4 [&>*]:w-full">
+               <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-[auto_auto] lg:grid-rows-[300px] lg:grid-cols-[2fr_1fr_1fr] gap-4 [&>*]:w-full">
                   <div className="bg-support06 rounded-3xl shadow min-w-0 min-h-[200px] md:min-h-[300px] lg:min-h-0"></div>
                   <div className="bg-support06 rounded-3xl shadow min-w-0 min-h-[200px] md:min-h-[300px] lg:min-h-0"></div>
                   <div className="grid grid-cols-2 md:grid-cols-2 md:col-span-2 lg:col-span-1 lg:grid-cols-1 gap-4 min-w-0">
@@ -17,7 +17,7 @@ export const Benefits: React.FC = () => {
 
                {/* row 2 - Ajustado grid para MD */}
                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-rows-[auto_auto] lg:grid-rows-[448px] lg:grid-cols-4 gap-4 [&>*]:w-full">
-                  <div className="grid gap-4 md:col-span-2 lg:col-span-1">
+                  <div className="grid gap-4 lg:grid-rows-[41%_20%_auto] md:col-span-2 lg:col-span-1">
                      <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                      <div className="grid grid-cols-2 gap-4">
                         <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
@@ -46,7 +46,7 @@ export const Benefits: React.FC = () => {
                </div>
 
                {/* row 3 - Ajustado grid para MD */}
-               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 md:grid-rows-[auto] lg:grid-rows-[138px] lg:grid-cols-[2fr_1.5fr_0.5fr] gap-4 [&>*]:w-full">
+               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 md:grid-rows-[auto] lg:grid-rows-[124px] lg:grid-cols-[2fr_1.5fr_0.5fr] gap-4 [&>*]:w-full">
                   <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] md:col-span-2 lg:col-span-1 lg:min-h-0"></div>
                   <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
                   <div className="bg-support06 h-full rounded-3xl shadow min-h-[100px] lg:min-h-0"></div>
