8e3b55697b2cc9ff830e151c6380fde4cdde9940
rafaeelricco <rafaelricco10@gmail.com>
2024-12-11 19:13:42 -0300

    fix: update PostCSS configuration to always include cssnano for production builds
    - Modified postcss.config.mjs to ensure cssnano is always included in the configuration, enhancing CSS optimization.
    - This change aims to improve the production build process by consistently applying CSS minification.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -6,7 +6,7 @@ const config = {
       tailwindcss: {},
       // See more about `cssnano` here: https://tailwindcss.com/docs/optimizing-for-production
       autoprefixer: {},
-      ...(process.env.APP_ENV === 'production' ? { cssnano: {} } : {})
+      cssnano: {}
    }
 }
 
