f088abb4181d53092a92d8a175b24c8f9b6d57ee
rafaeelricco <rafaelricco10@gmail.com>
2024-12-11 19:29:37 -0300

    refactor: simplify Next.js configuration by removing unused image settings
    - Removed the images configuration from next.config.ts to streamline the setup and focus on essential experimental features.
    - This change aims to enhance maintainability and clarity in the configuration file, aligning with recent optimizations in image handling.

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-   images: {
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      formats: ['image/avif', 'image/webp'],
-      minimumCacheTTL: 60
-   },
    experimental: {
-      turbo: {
-         minify: true,
-         useSwcCss: true
-      },
       serverMinification: true,
       optimizeCss: true,
       optimizePackageImports: ['@radix-ui/react-accordion', '@radix-ui/react-dialog'],
